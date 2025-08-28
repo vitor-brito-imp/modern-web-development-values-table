26c2222984c28b92a336e92c8ad78ba392c86a7e
Vitor Brito <vitoraraujo1694@gmail.com>
2025-08-27 22:40:25 -0300

    feat: Adicionar consumo de backend local

--- index.js
+++ index.js
@@ -1,76 +1,93 @@
-function Pessoa(altura, peso) {
-  if (!altura || !peso) {
-    throw new Error("Altura e peso são obrigatórios");
-  }
+class Pessoa {
+    constructor(altura, peso) {
+        if (!altura || !peso) {
+            throw new Error("Altura e peso são obrigatórios");
+        }
 
-  this.altura = altura;
-  this.peso = peso;
+        this.altura = altura;
+        this.peso = peso;
+    }
 }
 
-function Nutricionista(altura, peso) {
-  Pessoa.call(this, altura, peso);
-  this.imc = function () {
-    return this.peso / (this.altura * this.altura);
-  };
-
-  this.classificaIMC = function () {
-    var imc = this.imc();
-
-    var magreza = document.getElementById("magreza");
-    var normal = document.getElementById("normal");
-    var sobrepeso = document.getElementById("sobrepeso");
-    var obesidade = document.getElementById("obesidade");
-
-    magreza.style.backgroundColor = "transparent";
-    normal.style.backgroundColor = "transparent";
-    sobrepeso.style.backgroundColor = "transparent";
-    obesidade.style.backgroundColor = "transparent";
-
-    if (imc < 18.5) {
-      magreza.style.backgroundColor = "yellow";
-      return "Abaixo do peso";
-    }
-    if (imc >= 18.5 && imc < 24.9) {
-      normal.style.backgroundColor = "yellow";
-      return "Peso normal";
-    }
-    if (imc >= 25 && imc < 29.9) {
-      sobrepeso.style.backgroundColor = "yellow";
-      return "Sobrepeso";
+class Nutricionista extends Pessoa {
+    constructor(altura, peso) {
+        super(altura, peso);
+        this.valorImc = 0;
+        this.descricaoImc = "";
     }
 
-    obesidade.style.backgroundColor = "yellow";
-    return "Obesidade";
-  };
+    imc = async function () {
+        return calculaImc(this)
+            .then((imc) => {
+                console.log('----- Nutricionista->imc=> -----');
+                console.log(imc);
+                console.log(this);
+                this.valorImc = imc.imc;
+                this.descricaoImc = imc.imcDescription;
+            });
+    };
 }
-Nutricionista.prototype = Object.create(Pessoa.prototype);
-Nutricionista.prototype.constructor = Nutricionista;
+
+function calculaImc(nutricionista) {
+    return fetch("http://localhost:3000/imc/calculate", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ height: nutricionista.altura, weight: nutricionista.peso })
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Erro ao calcular IMC");
+            }
+        });
+}
+
+function renderizaTabelaIMC(imc) {
+   const intervalos = [
+       { min: 0, max: 18.4, classificacao: "Abaixo do peso" },
+       { min: 18.4, max: 24.9, classificacao: "Peso normal" },
+       { min: 24.9, max: 29.9, classificacao: "Sobrepeso" },
+       { min: 29.9, max: Infinity, classificacao: "Obesidade" }
+   ];
+
+   let html = "<table id='tabela-imc'><thead><tr><th>Classifica&ccedil;&atilde;o</th><th>IMC</th></tr></thead><tbody>";
+   intervalos.forEach((x) => {
+       const intervalo = x.min + " - " + x.max;
+       html += `<tr class='${(imc >= x.min && imc < x.max) ? "destaque-imc" : ""}'><td>${x.classificacao}</td><td>${intervalo}</td></tr>`
+   });
+   html += "</tbody></table>";
+   document.getElementById("tabela-imc-container").innerHTML = html;
+}
 
 function renderizaResultadoIMC(nutricionista) {
-  document.getElementById("imc").innerText =
-    nutricionista.imc().toFixed(2) + " - " + nutricionista.classificaIMC();
+    nutricionista.imc()
+        .then(() => {
+            document.getElementById("imc").innerText =
+                nutricionista.valorImc + " - " + nutricionista.descricaoImc;
+            renderizaTabelaIMC(parseFloat(nutricionista.valorImc));
+        });
 }
 
 function actionCalcularIMCBuilder() {
-  var alturaEl = document.getElementById("altura");
-  var pesoEl = document.getElementById("peso");
+    var alturaEl = document.getElementById("altura");
+    var pesoEl = document.getElementById("peso");
 
-  return function actionCalcularIMC(evt) {
-    evt.preventDefault();
+    return function actionCalcularIMC(evt) {
+        evt.preventDefault();
 
-    var nutricionista = new Nutricionista(
-      parseFloat(alturaEl.value),
-      parseFloat(pesoEl.value)
-    );
-    console.log(Nutricionista.prototype.constructor);
-    console.log(nutricionista instanceof Pessoa);
-
-    renderizaResultadoIMC(nutricionista);
-  };
+        const nutricionista = new Nutricionista(
+            parseFloat(alturaEl.value),
+            parseFloat(pesoEl.value)
+        );
+        renderizaResultadoIMC(nutricionista);
+    }
 }
 
 window.onload = function () {
-  document
-    .getElementById("calcular")
-    .addEventListener("click", actionCalcularIMCBuilder());
+    document
+        .getElementById("calcular")
+        .addEventListener("click", actionCalcularIMCBuilder());
 };
